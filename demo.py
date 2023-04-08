767fd7b15cf260554f5ad53aaa49ecbf0884de05
pablovela5620 <pablovela5620@gmail.com>
2023-04-08 14:44:58 -0500

    add rerun visualization

--- demo.py
+++ demo.py
@@ -14,8 +14,41 @@ from pytorch3d.io.obj_io import load_obj
 import main_mcc
 import mcc_model
 import util.misc as misc
-from engine_mcc import prepare_data, generate_html
+import rerun as rr
+import requests
+from pathlib import Path
+from engine_mcc import prepare_data
 
+MODEL_URL: str = "https://dl.fbaipublicfiles.com/MCC/co3dv2_all_categories.pth"
+
+def download_with_progress(url: str, dest: Path) -> None:
+    """Download file with tqdm progress bar."""
+    chunk_size = 1024 * 1024
+    resp = requests.get(url, stream=True)
+    total_size = int(resp.headers.get("content-length", 0))
+    with open(dest, "wb") as dest_file:
+        with tqdm(
+            desc="Downloading model", total=total_size, unit="iB", unit_scale=True, unit_divisor=1024
+        ) as progress:
+            for data in resp.iter_content(chunk_size):
+                dest_file.write(data)
+                progress.update(len(data))
+
+def occupancy_to_points(occupancy, xyz, color, threshold=0.1):
+
+    occupancy = torch.nn.Sigmoid()(occupancy)
+    pos = occupancy > threshold
+    points = xyz[pos].reshape((-1, 3))
+    features = color[None][pos].reshape((-1, 3))
+    good_points = points[:, 0] != -100
+
+    if good_points.sum() == 0:
+        return
+    
+    rr.log_points(
+        "world/points",
+        positions = points[good_points].cpu(),
+        colors = features[good_points].cpu())
 
 def run_viz(model, samples, device, args, prefix):
     model.eval()
@@ -31,6 +64,7 @@ def run_viz(model, samples, device, args, prefix):
     model.cached_enc_feat = None
     num_passes = int(np.ceil(unseen_xyz.shape[1] / max_n_unseen_fwd))
     for p_idx in tqdm(range(num_passes)):
+        rr.set_time_sequence("num_passes", p_idx+1)
         p_start = p_idx     * max_n_unseen_fwd
         p_end = (p_idx + 1) * max_n_unseen_fwd
         cur_unseen_xyz = unseen_xyz[:, p_start:p_end]
@@ -58,20 +92,15 @@ def run_viz(model, samples, device, args, prefix):
                     ) * torch.linspace(0, 1, 256, device=pred.device)
                 ).sum(axis=2)
             )
-    with open(prefix + '.html', 'a') as f:
-        generate_html(
-            None,
-            None, None,
-            torch.cat(pred_occupy, dim=1),
-            torch.cat(pred_colors, dim=0),
-            unseen_xyz,
-            f,
-            gt_xyz=None,
-            gt_rgb=None,
-            mesh_xyz=None,
-            score_thresholds=args.score_thresholds,
-            pointcloud_marker_size=3,
-        )
+
+        if p_idx != 0:
+            vis_occupy = torch.cat(pred_occupy, dim=1)
+            vis_colors = torch.cat(pred_colors, dim=0)
+            vis_xyz = unseen_xyz[:, :p_end, :]
+            occupancy_to_points(
+                vis_occupy,
+                vis_xyz,
+                vis_colors)
 
 def pad_image(im, value):
     if im.shape[0] > im.shape[1]:
@@ -101,6 +130,9 @@ def main(args):
     rgb = cv2.imread(args.image)
     obj = load_obj(args.point_cloud)
 
+    rr.set_time_sequence("num_passes", 0)
+    rr.log_image("input", rgb[..., ::-1])
+
     seen_rgb = (torch.tensor(rgb).float() / 255)[..., [2, 1, 0]]
     H, W = seen_rgb.shape[:2]
     seen_rgb = torch.nn.functional.interpolate(
@@ -113,7 +145,9 @@ def main(args):
     seen_xyz = obj[0].reshape(H, W, 3)
     seg = cv2.imread(args.seg, cv2.IMREAD_UNCHANGED)
     mask = torch.tensor(cv2.resize(seg, (W, H))).bool()
+    rr.log_tensor("mask", mask.float())
     seen_xyz[~mask] = float('inf')
+    rr.log_tensor("seen_xyz", seen_xyz)
 
     seen_xyz = normalize(seen_xyz)
 
@@ -156,17 +190,25 @@ if __name__ == '__main__':
     parser = main_mcc.get_args_parser()
     parser.add_argument('--image', default='demo/quest2.jpg', type=str, help='input image file')
     parser.add_argument('--point_cloud', default='demo/quest2.obj', type=str, help='input obj file')
-    parser.add_argument('--seg', default='demo/quest2_seg.png', type=str, help='input obj file')
+    parser.add_argument('--bbox-for-seg', default='demo/quest2_seg.png', type=str, help='input obj file')
     parser.add_argument('--output', default='demo/output', type=str, help='output path')
     parser.add_argument('--granularity', default=0.05, type=float, help='output granularity')
     parser.add_argument('--score_thresholds', default=[0.1, 0.2, 0.3, 0.4, 0.5], type=float, nargs='+', help='score thresholds')
     parser.add_argument('--temperature', default=0.1, type=float, help='temperature for color prediction.')
-    parser.add_argument('--checkpoint', default='co3dv2_all_categories.pth', type=str, help='model checkpoint')
 
     parser.set_defaults(eval=True)
-
+    rr.script_add_args(parser)
     args = parser.parse_args()
-    args.resume = args.checkpoint
+    rr.script_setup(args, "MCC")
+
+    # check that the checkpoint exists
+    checkpoint_path = Path("checkpoint") / "co3dv2_all_categories.pth"
+    if not checkpoint_path.exists():
+        download_with_progress(MODEL_URL, checkpoint_path)
+
+    args.resume = str(checkpoint_path)
     args.viz_granularity = args.granularity
+
     main(args)
+    rr.script_teardown(args)
 
